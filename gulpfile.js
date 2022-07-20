64ae309b3eefff48df71f3ca8b05bcd1cb0754d4
Baimuratov <ilvir.bajmuratov@mail.ru>
2022-07-20 16:10:04 +0300

    begin layout ballons

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+import gulp from "gulp";
+import browser from "browser-sync";
+import del from "del";
+import dartSass from "sass"
+import gulpSass from "gulp-sass";
+import tilde from "node-sass-tilde-importer";
+const sass = gulpSass(dartSass)
+
+const server = () => {
+	browser.init({
+		server: {
+			baseDir: `./dist`
+		},
+		notify: false,
+		port: 3000
+	})
+}
+
+const html = () => {
+	return gulp.src("./src/index.html")
+		.pipe(gulp.dest("./dist"))
+		.pipe(browser.stream())
+}
+
+const scss = () => {
+	return gulp.src("./src/scss/index.scss")
+		.pipe(sass({
+			importer: tilde
+		}))
+		.pipe(gulp.dest("./dist/css/"))
+		.pipe(browser.stream())
+}
+
+const img = () => {
+	return gulp.src("./src/img/**/*")
+		.pipe(gulp.dest("./dist/img/"))
+}
+
+const fonts = () => {
+	return gulp.src("./src/fonts/**/*")
+	.pipe(gulp.dest("./dist/fonts/"))
+}
+
+const clean = () => {
+	return del("./dist")
+}
+
+const watch = () => {
+	gulp.watch("./src/index.html", html)
+	gulp.watch("./src/scss/**/*.scss", scss)
+	gulp.watch("./src/img/**/*", img)
+
+}
+
+const main = gulp.parallel(fonts, img, html, scss)
+const dev = gulp.series(clean, main, gulp.parallel(watch, server))
+
+gulp.task("default", dev)
